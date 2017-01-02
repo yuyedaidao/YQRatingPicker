1afabc40af67a85115a0963db33370e43ced90d8
WangYQ <wyqpadding@gmail.com>
2017-01-02 16:11:56 +0800

    Initial Commit

---
+++ YQRatingPicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YQRatingPicker
+//
+//  Created by Wang on 2017/1/2.
+//  Copyright © 2017年 Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YQRatingPicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YQRatingPicker
+//
+//  Created by Wang on 2017/1/2.
+//  Copyright © 2017年 Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
